ab03586d1482fd568b525aeb80ba9383cb3f8ae2
CarolinaCarolOve <137331618+CarolinaCarolOve@users.noreply.github.com>
2026-05-28 18:34:08 -0300

    Add files via upload
    TP7 ; con el ejercicio 5 sin completar por falta de tiempo.

---
+++ TP7/ejercicio3.py
@@ -0,0 +1,4 @@
+nombres = [" mara ", "TOMAS", "  luCIA", "mARcos  ", " SOFIA "]
+nombres_normalizados=[nombre.strip().capitalize()for nombre in nombres]
+print(nombres_normalizados)
+
---
+++ TP7/ejercicio4.py
@@ -0,0 +1,10 @@
+edad= ((input("Ingresa tu edad: ")))
+if edad.strip().isnumeric():
+    eedad = int(edad)
+    if 0< eedad <120:
+        print(f"Edad registrada:{edad}")
+    else: 
+        print("Error, el dato ingresado no ha sido validado")
+else: 
+    print("Ingresa un número válido")
+ 
